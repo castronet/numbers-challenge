c39c30e10553d899a55cf39541b25f10802fe498
hex <hex@hex.cat>
2021-10-07 09:35:33 +0200

    Added File management Cleaned and ordered code

--- main.go
+++ main.go
@@ -15,48 +15,61 @@ import (
 /* Considerations
 * TPC/IP v4
 * Manage concurrent users with netutil // option to create a semaphore to do that
-
+* Channels for communication between go routines
 
  */
 
 func main() {
 
+    // [+] Variables declaration
 	maxConcurrentUsers := 5
+
+    // channels for communications
+	system := make(chan interface{}, 1)
+	input := make(chan int)
+
+	// mutex declaration to warrant right data management
+	mutex := sync.Mutex{}
+
+	// Variables to count numbers
+	numbers := map[int]struct{}{}
+	totalUniqueNumbers := 0
+	duplicatedNumbers := 0
+	uniqueNumbers := 0
+
+    // Open file where to write numbers
+    outputFile, err := os.OpenFile("./numbers.txt", os.O_RDWR|os.O_CREATE, 0666)
+    checkError(err)
+    defer outputFile.Close()
+
+    // Truncate the file and seek to the top
+    outputFile.Truncate(0)
+    outputFile.Seek(0,0)
+
 
 	// Open port
 	listener, err := net.Listen("tcp", ":4000")
 	checkError(err)
-	//    defer listener.Close()
 
+    // Define maximun concurrent users on our listener
 	listener = netutil.LimitListener(listener, maxConcurrentUsers)
 
-	system := make(chan interface{}, 1)
-	input := make(chan int)
-	//	connections := make(chan conn)
-
-	// my mutex
-	mutex := sync.Mutex{}
-
-	// a ordenar - inicialicazion
-	numbers := map[int]struct{}{}
-	totalNumbers := 0
-	duplicatedNumbers := 0
-	uniqueNumbers := 0
-	// open file - trucante
 
 	go func() {
 		for newNumber := range input {
 			// lock
 			mutex.Lock()
-			totalNumbers++
 			if _, ok := numbers[newNumber]; ok {
 				duplicatedNumbers++
 			} else {
 				uniqueNumbers++
+			    totalUniqueNumbers++
 				numbers[newNumber] = struct{}{}
 
-				// write to file
-				// file write newNumber
+                // Write to the file each number
+                _, err := outputFile.WriteString(fmt.Sprintf("%d\n", newNumber))
+//                    strconv.Itoa(number)
+                checkError(err)
 			}
 
 			//unlock
@@ -81,92 +94,52 @@ func main() {
 		tick := time.Tick(10 * time.Second)
 		for range tick {
 			mutex.Lock()
-			fmt.Printf("Received %d unique numbers, %d duplicates. Unique total: %d\n", uniqueNumbers, duplicatedNumbers, totalNumbers)
+
+			fmt.Printf("Received %d unique numbers, %d duplicates. Unique total: %d\n", uniqueNumbers, duplicatedNumbers, totalUniqueNumbers)
+
+            // initialize duplicate and unique range variables
 			duplicatedNumbers = 0
 			uniqueNumbers = 0
+
 			mutex.Unlock()
 		}
 	}()
 
 	<-system
 	close(system)
-	// todo el codigo de cleanup
 	listener.Close()
-	// can I close all go routines?
 
-	os.Exit(1)
+	os.Exit(0)
 }
 
 func handleClient(conn net.Conn, input chan int, system chan interface{}) {
-
-	// Valid inputs:
-	// 9 decimal number
-	// 123456789
-	// terminate
-	fmt.Println("Debug: handling connection")
+    // Open scanner to read data from clients
 	scanner := bufio.NewScanner(conn)
-
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		if len(line) > 9 { // check if the lenth must be 9 or 10
+        // Check if the input has the required lenght
+		if len(line) != 9 {
 			conn.Close()
+            return
 		}
 
+        // Convert a string to a int
+        // we did it at this point because we expect more correct numbers than errors
 		i, err := strconv.Atoi(line)
 		if err != nil {
+            // We check
 			// if here because of cost efficient just one terminate on
 			if line == "terminate" {
 				system <- struct{}{}
 			} else {
 				conn.Close()
 			}
-		}
-
-		input <- i
-
-		/*
-		   case "terminate":
-		       system <- struct{}{}
-
-		   default:
-		       if len(line) > 9 { // check if the lenth must be 9 or 10
-		           system <- struct{}{}
-		       }
-		*/
-
-		/*
-		   switch line {
-		       case "terminate":
-		           system <- struct{}{}
-
-		       default:
-		           if len(line) > 9 { // check if the lenth must be 9 or 10
-		               system <- struct{}{}
-		           }
-
-		           i, err := strconv.Atoi(line)
-		           input <- i
-		   }
-		*/
-//		fmt.Println("New line", line)
-
-		/*
-		   // check if the line is "terminate"
-		   if err != nil {
-		       system <- struct{}{}
-		   }
-		*/
-
+        } else {
+            input <- i
+        }
 	}
 
-	//  inputLength := 10
-
-	//  buffer := make([]byte, inputLength)
-
-	//		fmt.Println("test test close close handler")
-	// close connection on exit
-	//    connections <- conn
 	conn.Close()
 }
 
